814c3f7a694983f54ea40593c4fc7907dc90430e
sanvi <sanvis0184@gmail.com>
2024-08-21 15:50:38 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
